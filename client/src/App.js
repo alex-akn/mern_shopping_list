15fda4a02cf58f1d2c630cdf3ba43ad590fca0cb
Alexander <alex-akn@hotmail.com>
2018-07-09 16:58:24 +0300

    Redux finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
+import { Container } from "reactstrap";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
